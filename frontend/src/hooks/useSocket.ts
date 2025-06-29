fde1ad07da967c4247eb4f1533c50fbe0de1d3d8
tusharmangla19 <77486439+tusharmangla19@users.noreply.github.com>
2025-06-30 01:37:21 +0530

    Update useSocket.ts

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "react"
 
-const WS_URL = "ws://localhost:8081";
+const WS_URL = "chess-production-29d7.up.railway.app";
 
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
